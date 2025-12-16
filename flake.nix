03c0f355bfe80a5c9da260268ad1d835daa32f91
James Jackson <j.e.m.jackson@gmail.com>
2025-12-16 14:06:40 -0600

    Changed dependency for build shell from gdb to lldb

--- flake.nix
+++ flake.nix
@@ -1,28 +1,29 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  outputs =
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    outputs =
     { self
-    , nixpkgs
-    ,
+        , nixpkgs
+            ,
     }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+        system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
     in
     {
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-      devShells.${system}.default =
-        pkgs.mkShell {
-          packages = [
-            pkgs.aoc-cli
-            pkgs.cargo
-            pkgs.rustc
-            pkgs.rust-analyzer
-            pkgs.gdb
-	    pkgs.nodejs
-	    pkgs.rustfmt		
-          ];
-        };
+        formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+        devShells.${system}.default =
+            pkgs.mkShell {
+                name = "advent-2022-rs shell";
+                packages = [
+                    pkgs.aoc-cli
+                        pkgs.cargo
+                        pkgs.rustc
+                        pkgs.rust-analyzer
+                        pkgs.lldb
+                        pkgs.nodejs
+                        pkgs.rustfmt		
+                ];
+            };
     };
 }
 
